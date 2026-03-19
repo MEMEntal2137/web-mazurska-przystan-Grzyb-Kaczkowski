200d9ed7ac7268f7648f034adb44c8c60428dc53
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-03-19 19:09:22 +0100

    Add handleSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,42 @@ function App() {
         setRange(Number(e.target.value));
     }
 
+
+  
+const [price, setPrice] = useState<number>(0);
+  function handleSubmit(e: SyntheticEvent<HTMLFormElement>) {
+         {
+            e.preventDefault();
+
+            const form = e.currentTarget;
+
+            const boat = (form.elements.namedItem("boat") as HTMLSelectElement).value;
+            const kapok = (form.elements.namedItem("kapok") as HTMLInputElement).checked;
+            const kurs = (form.elements.namedItem("kurs") as HTMLInputElement).checked;
+
+            let total = 0;
+
+            let zaGodzine = 0;
+
+            switch (boat) {
+                case "boat":
+                    zaGodzine = 20;
+                    break;
+                case "rower":
+                    zaGodzine = 35;
+                    break;
+                case "omega":
+                    zaGodzine = 150;
+                    break;
+            }
+
+            total += zaGodzine * range;
+
+            if (kapok) total += 5;
+            if (kurs) total += 50 * range;
+
+            setPrice(total);
+
   return (
     <>
 
