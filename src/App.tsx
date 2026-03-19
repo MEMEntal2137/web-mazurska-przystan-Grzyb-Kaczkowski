336f2e6774e13e5c0240df5a953b442a4f05436d
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-03-19 19:36:32 +0100

    Add required values

--- src/App.tsx
+++ src/App.tsx
@@ -56,67 +56,68 @@ Instruktor: ${kurs ? "tak" : "nie"}`);
 
   return (
     <>
- <>
-        <Organizer>
-          <form onSubmit={handleSubmit}>
-            <input
-            type="text"
-            id="nazwa"
-            placeholder="Wpisz swoje imie"/>
-            <br/>
-            <select name="boat" id="boat">
-              <option value="none">Wypożyczany transport</option>
-              <option value="boat">Kajak(20zł/h)</option>
-              <option value="rower">Rower wodny(35zł/h)</option>
-              <option value="omega">Omega(150zł/h)</option>
-            </select>
-            <br/>
-            <label style={{display: "flex", flexDirection: "row"}}>
-              <p>Godziny</p>
-            <input
-            type="range"
-            name="hours"
-            onChange={handleRangeChange}
-            step="1"
-            max="8"
-            min="1"
-            />
-              <p id="rangeId">1</p>
-            </label>
+    <Organizer>
+        <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        id="nazwa"
+                        placeholder="Wpisz swoje imie"
+                        value={name}
+                    <br/>
+                    <select name="boat" id="boat">
+                        <option value="none">Wypożyczany transport</option>
+                        <option value="boat">Kajak(20zł/h)</option>
+                        <option value="rower">Rower wodny(35zł/h)</option>
+                        <option value="omega">Omega(150zł/h)</option>
+                    </select>
+                    <br/>
+                    <label style={{display: "flex", flexDirection: "row"}}>
+                        <p>Godziny</p>
+                        <input
+                            type="range"
+                            name="hours"
+                            onChange={handleRangeChange}
+                            step="1"
+                            max="8"
+                            min="1"
+                            value={range}
+                        />
+                        <p id="rangeId">{range}</p>
+                    </label>
 
-            <label>
-              <input
-              type="checkbox"
-              name="kapok"
-              />
-              kapok(5zł)
-            </label>
-            <br/>
-            <label>
-              <input
-                  type="checkbox"
-                  name="kurs"
-              />
-              instruktor(50zł/h)
-            </label>
-            <br/>
-            <label>
-            <input
-            type="radio"
-            name="payment"
-            value="paypal"
-            />
-              PayPal
-            </label>
-            <br/>
-            <label>
-              <input
-                  type="radio"
-                  name="payment"
-                  value="blik"/>
-              Blik
-            </label>
-            <br/>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="kapok"
+                        />
+                        kapok(5zł)
+                    </label>
+                    <br/>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="kurs"
+                        />
+                        instruktor(50zł/h)
+                    </label>
+                    <br/>
+                    <label>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="paypal"
+                        />
+                        PayPal
+                    </label>
+                    <br/>
+                    <label>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="blik"/>
+                        Blik
+                    </label>
+                    <br/>
             <button type="submit">Rezerwuję</button>
           </form>
           <br/>
